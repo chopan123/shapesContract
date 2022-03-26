aa01d03187146f8abcc1294fe2f7a25744c6ed12
chopan123 <chopancatrileo@gmail.com>
2022-03-26 12:46:30 -0300

    gas fixed

--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -25,6 +25,7 @@ module.exports = {
             );
          },
          network_id: 1337,
+         gasPrice: 200000000000
       },
       // Moonbase Alpha TestNet
       moonbase: {
@@ -51,6 +52,13 @@ module.exports = {
    compilers: {
       solc: {
          version: '^0.8.9',
+         settings: {          // See the solidity docs for advice about optimization and evmVersion
+           optimizer: {
+             enabled: true,
+             runs: 200
+           },
+           // evmVersion: "byzantium"
+         }
       },
    },
    // Moonbeam Truffle Plugin & Truffle Plugin for Verifying Smart Contracts
